29a12db2e3648c161201b92b34042bafcec9f6bc
Dominik Kotecki <Dominik.Kotecki@rossmann.com.pl>
2022-03-30 11:04:39 +0200

    add linq test

---
+++ src/LinqTest/MultipleEnumerationsBenchmark.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BenchmarkDotNet.Attributes;
+
+namespace LinqTest
+{
+    [MemoryDiagnoser]
+    public class MultipleEnumerationsBenchmark
+    {
+        private List<int> _list;
+        private IEnumerable<int> _enumerable;
+
+        public MultipleEnumerationsBenchmark()
+        {
+            _enumerable = Enumerable.Range(0, 100);
+            _list = Enumerable.Range(0, 100).ToList();
+        }
+        
+        [Benchmark]
+        public void MultipleEnumerations()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                foreach (var item in _enumerable)
+                {
+
+                }
+            }
+        }
+
+        [Benchmark]
+        public void List()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                foreach (var item in _list)
+                {
+
+                }
+            }
+        }
+    }
+}
