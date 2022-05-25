2d091bbd9878bc920832d85e087a02d230428dfd
Dominik Kotecki <Dominik.Kotecki@rossmann.com.pl>
2022-05-25 14:35:37 +0200

    add simple checksum generator

---
+++ src/JsonSerialization/Checksum/ChecksumGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Cryptography;
+using System.Text.Json;
+
+namespace JsonSerialization.Checksum
+{
+    internal static class ChecksumGenerator
+    {
+        public static string GetChecksum(Person person)
+        {
+            var json = JsonSerializer.SerializeToUtf8Bytes(person, BenchmarkJsonContext.Default.Person);
+            return Convert.ToHexString(MD5.HashData(json));
+        }
+    }
+}
--- src/JsonSerialization/Program.cs
+++ src/JsonSerialization/Program.cs
@@ -2,7 +2,14 @@
 using BenchmarkDotNet.Running;
 
 using JsonSerialization;
+using JsonSerialization.Checksum;
 
 Console.WriteLine("Hello, World!");
 
-var summary = BenchmarkRunner.Run<SerializationBenchmark>();
+//var summary = BenchmarkRunner.Run<SerializationBenchmark>();
+
+var person1 = new Person() { Age = 21, Name = "xD" };
+var person2 = new Person() { Age = 21, Name = "xD" };
+
+
+Console.WriteLine($"{ChecksumGenerator.GetChecksum(person1)} === {ChecksumGenerator.GetChecksum(person2)}");
