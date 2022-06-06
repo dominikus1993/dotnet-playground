4641dc9d1c78a335d805f7f44a5fcdd52a23c983
Dominik Kotecki <Dominik.Kotecki@rossmann.com.pl>
2022-06-06 12:34:07 +0200

    add json streaming

--- src/JsonSerialization/Program.cs
+++ src/JsonSerialization/Program.cs
@@ -1,8 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using BenchmarkDotNet.Running;
+using System.Text.Json;
 
 using JsonSerialization;
 using JsonSerialization.Checksum;
+
 
 Console.WriteLine("Hello, World!");
 
@@ -13,3 +14,18 @@ var person2 = new Person() { Age = 21, Name = "xD" };
 
 
 Console.WriteLine($"{ChecksumGenerator.GetChecksum(person1)} === {ChecksumGenerator.GetChecksum(person2)}");
+
+var persons = new[] { new Person { Age = 2, Name = "xD" } };
+
+var jsonB = JsonSerializer.SerializeToUtf8Bytes(persons);
+
+await File.WriteAllBytesAsync("./tst", jsonB);
+
+
+using var jsonS = new FileStream("./tst", FileMode.Open);
+var personsFomFile = JsonSerializer.DeserializeAsyncEnumerable<Person>(jsonS);
+
+await foreach (var item in personsFomFile)
+{
+    Console.WriteLine($"{item.Name} - {item.Age}");
+}
