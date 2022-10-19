b6acbfb321b5d05f8ea84f2bdf141a0797a134fa
Dominik Kotecki <Dominik.Kotecki@rossmann.com.pl>
2022-10-19 13:42:52 +0200

    test

--- src/LinqTest/Program.cs
+++ src/LinqTest/Program.cs
@@ -2,7 +2,40 @@
 using BenchmarkDotNet.Running;
 
 using LinqTest;
+using static LanguageExt.Prelude;
 
 Console.WriteLine("Hello, World!");
 
-var summary = BenchmarkRunner.Run<MultipleEnumerationsBenchmark>();
+//var summary = BenchmarkRunner.Run<MultipleEnumerationsBenchmark>();
+
+var a = Some(1);
+
+
+switch (a.Case)
+{
+    case int b:
+        Console.WriteLine($"xDDD {b}");
+        break;
+}
+
+var e = Right<Exception, IReadOnlyList<int>>(new List<int>() { 1 });
+
+switch (e.Case)
+{
+    case IReadOnlyList<int> l:
+        Console.WriteLine($"spierdalaj {l.Count}");
+        break;
+    case Exception exc:
+        Console.WriteLine("wypierdalaj");
+        break;
+}
+
+Console.WriteLine($"A: {ImportSteps.Categories.ToString()}");
+
+enum ImportSteps
+{
+    Synchronize = 1,
+    Products = 5,
+    Categories = 6,
+    Solr = 8
+}
