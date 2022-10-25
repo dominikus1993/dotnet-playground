c49ad24d57a79fe86c789885b54a6d31a1c9b435
Dominik Kotecki <Dominik.Kotecki@rossmann.com.pl>
2022-10-25 15:49:53 +0200

    .

--- src/FunctionalProgramming/Program.cs
+++ src/FunctionalProgramming/Program.cs
@@ -1,3 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using FunctionalProgramming;
+
+using LanguageExt;
+
+using static LanguageExt.Prelude;
+
+var names = new[] { "SomeFlag" };
+
+var res = TestHelper.GetStatuses(names);
+
+Option<string> txt = Optional<string>(null).Where(x => x.Length > 0);
+
+Console.WriteLine($"Hello, World! {res.Contains(Test.SomeFlag)} {txt}");
---
+++ src/FunctionalProgramming/Test.cs
@@ -0,0 +1,23 @@
+﻿namespace FunctionalProgramming;
+
+public enum Test
+{
+    SomeFlag,
+    AnotherFlag
+}
+
+public class TestHelper
+{
+    public static IReadOnlySet<Test> GetStatuses(IEnumerable<string> names)
+    {
+        var set = new HashSet<Test>();
+        foreach (var attribute in names)
+        {
+            if (Enum.TryParse(attribute, true, out Test status))
+            {
+                set.Add(status);
+            }
+        }
+        return set;
+    }
+}
