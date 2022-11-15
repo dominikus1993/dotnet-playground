8486e430282aa7d58b9ee8138304b749e4cab23e
Dominik Kotecki <Dominik.Kotecki@rossmann.com.pl>
2022-11-15 14:19:21 +0100

    .

--- src/HttpClient/Program.cs
+++ src/HttpClient/Program.cs
@@ -1,3 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using System.Net;
+
+var httpClientHandler = new SocketsHttpHandler()
+{
+    AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip,
+    PooledConnectionLifetime = TimeSpan.FromSeconds(6),
+    PooledConnectionIdleTimeout = TimeSpan.FromMinutes(5),
+};
+
+var client = new HttpClient(httpClientHandler);
+
+            
+for (var i = 0; i < 50; i++)
+{
+    var resp = await client.GetAsync("https://www.google.com");
+    Console.WriteLine($"Sent: {resp.StatusCode}");
+    await Task.Delay(TimeSpan.FromSeconds(2));
+}
+
+Console.WriteLine("Press a key to exit...");
+Console.ReadKey();
