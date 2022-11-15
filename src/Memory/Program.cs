7428d85157bf259884bdd3fb7e246528620a56c1
Dominik Kotecki <Dominik.Kotecki@rossmann.com.pl>
2022-11-15 15:34:47 +0100

    small changes

--- src/Memory/Program.cs
+++ src/Memory/Program.cs
@@ -1,21 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Buffers;
+
+using BenchmarkDotNet.Running;
+
+using Memory;
+
 using Microsoft.IO;
 
-Console.WriteLine("Hello, World!");
 
-RecyclableMemoryStreamManager manager = new RecyclableMemoryStreamManager()
-{
-    
-};
-
-for (int i = 0; i < 10; i++)
-{
-    await using var stream = File.OpenRead("./jp2137.jpg");
-    using var memory = manager.GetStream();
-    await stream.CopyToAsync(memory);
-    var byteA = memory.ToArray();
-    Console.WriteLine(byteA.Length);
-}
-
-Console.WriteLine(manager.BlockSize);
+var summary = BenchmarkRunner.Run<BenchmarkBenchmark>();
---
+++ src/Memory/StreamBenchmark.cs
@@ -0,0 +1,40 @@
+﻿using System.Buffers;
+
+using BenchmarkDotNet.Attributes;
+
+using Microsoft.Extensions.Logging;
+using Microsoft.IO;
+
+namespace Memory;
+
+
+[MemoryDiagnoser]
+public class BenchmarkBenchmark
+{
+    private static readonly RecyclableMemoryStreamManager manager = new RecyclableMemoryStreamManager()
+    {
+    
+    };
+
+    private Stream FileStream;
+
+    public BenchmarkBenchmark()
+    {
+        FileStream = File.OpenRead("./jp2137.jpg");
+    }
+    [Benchmark]
+    public async Task<byte[]> MemoryStream()
+    {
+        await using var memory = new MemoryStream();
+        await FileStream.CopyToAsync(memory!);
+        return memory.ToArray();
+    }
+
+    [Benchmark]
+    public async Task<byte[]> RecyclableMemoryStreamManager()
+    {
+        await using var memory = manager.GetStream() as RecyclableMemoryStream;
+        await FileStream.CopyToAsync(memory!);
+        return memory.ToArray();
+    }
+}
