f8d9de930183833e2e2b94f327e40e487ae34c51
Dominik Kotecki <Dominik.Kotecki@rossmann.com.pl>
2022-12-16 10:55:16 +0100

    add json coverter

--- src/JsonSerialization/Person.cs
+++ src/JsonSerialization/Person.cs
@@ -1,6 +1,30 @@
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace JsonSerialization;
+
+[JsonConverter(typeof(NameStringJsonConverter))]
+public readonly record struct Name(string Value);
+
+public class NameStringJsonConverter : JsonConverter<Name>
+{
+    public override Name Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (reader.TokenType != JsonTokenType.String)
+        {
+            throw new JsonException("Name should be string");
+        }
+
+        var value = reader.GetString();
+
+        return new Name(value!);
+    }
+
+    public override void Write(Utf8JsonWriter writer, Name value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.Value);
+    }
+}
 
 [JsonSerializable(typeof(Person))]
 [JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase)]
@@ -14,7 +38,7 @@ public class Tag
 }
 public class Person
 {
-    public string? Name { get; set; }
+    public Name Name { get; set; }
     public int Age { get; set; }
     
     public IReadOnlyList<Tag> Tags { get; set; }
