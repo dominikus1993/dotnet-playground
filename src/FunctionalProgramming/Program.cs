416e34486376c2b8900ad8504543c65e328625ca
Dominik Kotecki <Dominik.Kotecki@rossmann.com.pl>
2023-02-08 15:59:12 +0100

    fix

--- src/FunctionalProgramming/Program.cs
+++ src/FunctionalProgramming/Program.cs
@@ -1,62 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using BenchmarkDotNet.Running;
+
 using FunctionalProgramming;
 
-using LanguageExt;
+Console.WriteLine("Hello, World!");
 
-using static LanguageExt.Prelude;
-
-var names = new[] { "SomeFlag" };
-
-var res = TestHelper.GetStatuses(names);
-
-Option<string> txt = await OptionTests.Test2(null);
-
-var result = B();
-
-switch (result.Case)
-{
-    case Exception exception:
-        Console.WriteLine("Blad");
-        break;
-    default:
-        Console.WriteLine("xDDDD");
-        break;
-}
-
-Either<Exception, Unit> B()
-{
-    try
-    {
-        return Right(Unit.Default);
-    }
-    catch (Exception e)
-    {
-        return Left<Exception, Unit>(new MyTestException("xDDD"));
-    }
-}
-
-IEnumerable<int> A()
-{
-    foreach (var value in Enumerable.Range(1, 10))
-    {
-        if (value % 2 == 0)
-        {
-            yield return value;
-            continue;
-        }
-
-        yield return value * 2;
-    }
-}
-
-class MyTestException : Exception
-{
-    public MyTestException(string? message) : base(message)
-    {
-    }
-
-    public MyTestException(string? message, Exception? innerException) : base(message, innerException)
-    {
-    }
-}
+var summary = BenchmarkRunner.Run<TestAnyInWhereVsContainsInHashSet>();
---
+++ src/FunctionalProgramming/TestAnyInWhereVsContainsInHashSet.cs
@@ -0,0 +1,36 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace FunctionalProgramming;
+
+public class Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+[MemoryDiagnoser()]
+public class TestAnyInWhereVsContainsInHashSet
+{
+    private List<Person> _list;
+    public TestAnyInWhereVsContainsInHashSet()
+    {
+        _list = Enumerable.Range(0, 500).Select(x => new Person() { Id = x, Name = Guid.NewGuid().ToString()}).ToList();
+    }
+        
+    [Benchmark]
+    public List<Person> Contains()
+    {
+        var personsFromDb = Enumerable.Range(20, 10).Select(x => new Person() { Id = x, Name = Guid.NewGuid().ToString()}).ToList();
+        var dict = personsFromDb.Select(x => x.Id).ToHashSet();
+
+        return _list.Where(x => dict.Contains(x.Id)).ToList();
+    }
+
+    [Benchmark]
+    public List<Person> ListAny()
+    {
+        var personsFromDb = Enumerable.Range(20, 10).Select(x => new Person() { Id = x, Name = Guid.NewGuid().ToString()}).ToList();
+
+        return _list.Where(x => personsFromDb.Any(y => y.Id == x.Id)).ToList();
+    }
+}
