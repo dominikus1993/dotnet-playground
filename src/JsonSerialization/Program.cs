5c1505914107aa70923568ed03895c30ce687399
Dominik Kotecki <Dominik.Kotecki@rossmann.com.pl>
2023-02-22 10:06:12 +0100

    fix

--- src/JsonSerialization/Program.cs
+++ src/JsonSerialization/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using BenchmarkDotNet.Running;
+
 using JsonSerialization;
 using JsonSerialization.Checksum;
 
@@ -10,30 +12,4 @@ using JsonSerializer = System.Text.Json.JsonSerializer;
 
 Console.WriteLine("Hello, World!");
 
-//var summary = BenchmarkRunner.Run<SerializationBenchmark>();
-
-var person1 = new Person() { Age = 21, Name = new Name("xD") };
-var person2 = new Person() { Age = 21, Name = new Name("xD")};
-
-
-Console.WriteLine($"{ChecksumGenerator.GetChecksum(person1)} === {ChecksumGenerator.GetChecksum(person2)}");
-
-var persons = new[] { new Person { Age = 2, Name = new Name("xD"), Tags = new []{new Tag() { Value = "xD"}}} };
-
-var jsonB = JsonSerializer.SerializeToUtf8Bytes(persons);
-
-await File.WriteAllBytesAsync("./tst", jsonB);
-
-
-await using var jsonS = new FileStream("./tst", FileMode.Open);
-var personsFomFile = JsonSerializer.DeserializeAsyncEnumerable<Person>(jsonS);
-
-await foreach (var item in personsFomFile)
-{
-    Console.WriteLine($"{item.Name} - {item.Age}");
-}
-
-var json = JsonSerializer.Serialize(persons);
-Console.WriteLine(json);
-
-Console.WriteLine(JsonSerializer.Deserialize<Person[]>(json)[0].Tags[0].Value);
+var summary = BenchmarkRunner.Run<SerializationBenchmark>();
--- src/JsonSerialization/SerializationBenchmark.cs
+++ src/JsonSerialization/SerializationBenchmark.cs
@@ -1,3 +1,5 @@
+using System.Text.Json;
+
 using BenchmarkDotNet.Attributes;
 
 namespace JsonSerialization;
@@ -5,26 +7,27 @@ namespace JsonSerialization;
 [MemoryDiagnoser]
 public class SerializationBenchmark
 {
+    private readonly JsonSerializerOptions Options = JsonSerializerOptions.Default;
     private readonly Person _person = new Person { Name = new Name("John Doe"), Age = 42, Tags = new []{new Tag(){Value = "xD"}}};
     
     [Benchmark]
-    public void NewtonsoftBenchmark()
+    public Person NewtonsoftBenchmark()
     {
         var json = Newtonsoft.Json.JsonConvert.SerializeObject(_person);
-        Newtonsoft.Json.JsonConvert.DeserializeObject<Person>(json);
+        return Newtonsoft.Json.JsonConvert.DeserializeObject<Person>(json);
     }
 
     [Benchmark]
-    public void SystemTextJsonBenchmark()
+    public Person SystemTextJsonBenchmark()
     {
-        var json = System.Text.Json.JsonSerializer.Serialize(_person);
-        System.Text.Json.JsonSerializer.Deserialize<Person>(json);
+        var json = System.Text.Json.JsonSerializer.Serialize(_person, Options);
+        return System.Text.Json.JsonSerializer.Deserialize<Person>(json, Options);
     }
 
     [Benchmark]
-    public void SystemTextJsonSourceGenBenchmark()
+    public Person SystemTextJsonSourceGenBenchmark()
     {
         var json = System.Text.Json.JsonSerializer.Serialize(_person, BenchmarkJsonContext.Default.Person);
-        System.Text.Json.JsonSerializer.Deserialize<Person>(json, BenchmarkJsonContext.Default.Person);
+        return System.Text.Json.JsonSerializer.Deserialize<Person>(json, BenchmarkJsonContext.Default.Person);
     }
 }
