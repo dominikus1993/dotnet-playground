c4e2ccda99f79dc26fcd086a0e2fed42731bdbac
Dominik.Kotecki <dominikus1910@gmail.com>
2023-03-24 18:30:59 +0100

    fix

--- src/Memory/StringBenchmark.cs
+++ src/Memory/StringBenchmark.cs
@@ -7,20 +7,43 @@ namespace Memory;
 [MemoryDiagnoser]
 public class StringBenchmark
 {
-    private const string text = "Jana Pawła 2137 jp2gmd xDDDDDDDDDDDDDDDDDDD ja jebie xDDDD";
+    private const string text = "Jana Pawła 2137 jp2gmd xDDDDDDDDDDDDDDDDDDD ja jebie xDDDD  xD jann pawlacz drugi";
 
     private static readonly Dictionary<char, char> replacements =
         new Dictionary<char, char>() { { 'J', 'j' }, { 'a', 'A' }, { 'n', 'N' }, { 'P', 'p' }, { '3', '7' }, { 'x', 'X' }, { 'D', 'd' } };
 
 
+    private static readonly string[] remove = { "1", "x", "D", "j" };
+    
     [Benchmark]
     public string SlowKey()
     {
         return text.SlowReplace(replacements);
     } 
+    
+    [Benchmark]
+    public string SlowSbKey()
+    {
+        return text.SlowStringBuilderReplace(replacements);
+    } 
+    
+    
     [Benchmark]
     public string FastKey()
     {
         return text.FastReplace(replacements);
+    } 
+    
+    [Benchmark]
+    public string SlowRemove()
+    {
+        return text.SlowRemove(remove);
+    } 
+    
+    
+    [Benchmark]
+    public string FastRemove()
+    {
+        return text.FastRemove(remove);
     } 
 }
--- src/Memory/StringUtils.cs
+++ src/Memory/StringUtils.cs
@@ -1,22 +1,26 @@
 ﻿using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Memory;
 
 public static class StringUtils
 {
+
     public static string FastReplace(this string str, Dictionary<char, char> replacements)
     {
         Span<char> textChars = stackalloc char[str.Length];
         str.CopyTo(textChars);
-        for (int i = 0; i < textChars.Length; i++)
+        int i = 0;
+        foreach(var element in textChars)
         {
-            var element = textChars[i];
             ref char replacement = ref CollectionsMarshal.GetValueRefOrNullRef(replacements, element);
             if (!Unsafe.IsNullRef(ref replacement))
             {
                 textChars[i] = replacement;
             }
+            i++;
         }
         return new string(textChars);
     }
@@ -29,6 +33,35 @@ public static class StringUtils
             result = result.Replace(repl.Key, repl.Value);
         }
         return result;
+    }
+    public static string SlowStringBuilderReplace(this string str, IReadOnlyDictionary<char, char> replacements)
+    {
+        var result = new StringBuilder(str);
+        foreach (var repl in replacements)
+        {
+            result = result.Replace(repl.Key, repl.Value);
+        }
+        return result.ToString();
+    }
+    
+    public static string FastRemove(this string str, IEnumerable<string> replacements)
+    {
+        var result = str;
+        foreach (var repl in replacements)
+        {
+            result = result.Replace(repl, string.Empty, StringComparison.InvariantCulture);
+        }
+        return result;
+    }
+    
+    public static string SlowRemove(this string str, IEnumerable<string> replacements)
+    {
+        var result = new StringBuilder(str);
+        foreach (var repl in replacements)
+        {
+            result = result.Replace(repl, string.Empty);
+        }
+        return result.ToString();
     }
     
 }
